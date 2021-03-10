b55065fae5135484996a76062bab9f94eecfb098
Nuno Fachada <nfachada@laseeb.org>
2021-03-10 15:54:06 +0000

    Adicionar exercícios resolvidos da aula 3

---
+++ Aula03/ArrayBiDim/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ArrayBiDim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+            float[,] matrix;
+            int rows, cols;
+            float sumOfMeans = 0;
+
+            // Ask user for array dimensions
+            Console.Write("Number of rows : ");
+            rows = int.Parse(Console.ReadLine());
+            Console.Write("Number of cols : ");
+            cols = int.Parse(Console.ReadLine());
+
+            // Create the array with the sizes given by user
+            matrix = new float[rows, cols];
+
+            // Ask the user for a number to put in each position
+            // of the array
+            for (int r = 0; r < matrix.GetLength(0); r++)
+            {
+                for (int c = 0; c < matrix.GetLength(1); c++)
+                {
+                    string aux;
+                    Console.Write($"Value in ({r},{c}): ");
+                    aux = Console.ReadLine();
+                    matrix[r, c] = float.Parse(aux);
+                }
+            }
+
+            // Calculate the mean of the values in each line
+            // and show it
+            for (int r = 0; r < matrix.GetLength(0); r++)
+            {
+                float aux = 0;
+                for (int c = 0; c < matrix.GetLength(1); c++)
+                {
+                    aux += matrix[r, c];
+                }
+                aux /= matrix.GetLength(1);
+                Console.WriteLine($"Mean of row {r} : {aux}");
+                sumOfMeans += aux;
+            }
+
+            // Calculate the sum of the means and show it
+            Console.WriteLine($"Sum of means is {sumOfMeans}");
+        }
+    }
+}
---
+++ Aula03/PercorreString/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PercorreString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+            string inputString;
+            char inputChar;
+
+            // Ask user for a string
+            Console.Write("Write a string: ");
+            inputString = Console.ReadLine();
+
+            // Ask user for a character
+            Console.Write("Write a char  : ");
+            // inputChar = Convert.ToChar(Console.ReadLine());
+            // inputChar = char.Parse(Console.ReadLine());
+            inputChar = Console.ReadLine()[0];
+
+            // Write the string without the char
+            foreach (char c in inputString)
+            {
+                if (c != inputChar)
+                {
+                    Console.Write(c);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
