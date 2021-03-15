459f7c73442b7a7dde94a8f9180fe07248c6b900
Nuno Fachada <faken@fakenmc.com>
2021-03-15 22:47:42 +0000

    Adicionar ficheiros exemplo para a aula 04

---
+++ Aula04/FamousQuotes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Aula04
+{
+    class FamousQuotes
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You talking to me?");
+            MyPrecious();
+            Console.WriteLine("Dodge this");
+            Hasta();
+        }
+
+        static void Hasta()
+        {
+            TheMoney();
+            Console.WriteLine("Hasta la vista, baby.");
+            MyPrecious();
+        }
+
+        static void TheMoney()
+        {
+            Console.WriteLine("Show me the money!");
+        }
+
+        static void MyPrecious()
+        {
+            TheMoney();
+            Console.WriteLine("My precious.");
+        }
+    }
+}
