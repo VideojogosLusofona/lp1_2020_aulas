2e3ac14820620d4feea98a872035bc14b51af38d
Nuno Fachada <faken@fakenmc.com>
2021-03-24 12:46:32 +0000

    Adicionar exercícios resolvidos aula 5

---
+++ Aula05/Exercicio4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Chronos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaracao de variaveis
+            Stopwatch cron1, cron2;
+
+            // Inicializar cronometros
+            cron1 = new Stopwatch();
+            cron2 = new Stopwatch();
+
+            cron1.Start();
+            Thread.Sleep(1000);
+
+            cron2.Start();
+            Thread.Sleep(1000);
+
+            cron1.Stop();
+            cron2.Stop();
+
+            Console.WriteLine(
+                $"Tempo cron1 = {cron1.ElapsedMilliseconds}");
+            Console.WriteLine(
+                $"Tempo cron2 = {cron2.ElapsedMilliseconds}");
+        }
+    }
+}
---
+++ Aula05/Exercicio6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyTestOfRandoms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string aux;
+            int n, soma = 0;
+            Random rnd = new Random();
+
+            Console.Write("Insira nº de dados: ");
+            aux = Console.ReadLine();
+            n = int.Parse(aux);
+
+            for (int i = 0; i < n; i++)
+            {
+                int valor = rnd.Next(1, 7);
+                Console.WriteLine($"Dado {i} com valor {valor}");
+                soma += valor;
+            }
+            Console.WriteLine($"Soma = {soma}");
+        }
+    }
+}
